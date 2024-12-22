a35dbecf9715930dc3de65ee9961afad7192c6b4
Khánh28- <nguyendanhnamkhanh06@gmail.com>
2024-12-23 06:55:18 +0700

    Add files via upload

---
+++ bai12ss6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	
+	int n; 
+	printf("Nhap vao gia tri n: ");
+	scanf("%d", &n);
+	int num1= 0;
+	int num2=1;
+	int fn;
+	for(int i=0; i<n; i++){
+	
+		printf("%d\n", n1);
+		fn= n1+n2;
+		num1 = num2 ;
+		num2 =fn;
+	}
+	return 0;
+}
+
